6aacc90fb409b7238afe90a9c11c02639ea461c4
alexey-chvarkov <csharp.worker@gmail.com>
2016-09-22 21:27:52 +0300

    Add output SideItems in AdminPanel

--- App/Views/Admin/SideManagerView.php
+++ App/Views/Admin/SideManagerView.php
@@ -1,24 +1,39 @@
 <?php use App\Application as Application; ?>
 
-<h1>Side manager</h1>
-
-<div class="container">
-    <a href="?p=<?php echo Application::$Configuration->AdminPage; ?>&open=side_manager_add">Add new item</a>
-</div>
-
-<?php foreach (Application::$Database->SideItems as $SideItem): ?>
-    <div class="sideitem">
-        <form name="side-item_<?php echo $SideItem->getValue()->SideItemId; ?>" method="post">
-            <div class="sideitem-col">
-                <span><?php echo $SideItem->getValue()->Title; ?></span>
-            </div>
-            <div class="sideitem-nav">
-                <input name="side-item-change_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="✎" />
-                <input name="side-item-inc_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="▲" />
-                <input name="side-item-dec_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="▼" />
-                <input name="side-item-del_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="✖" /> 
-            </div>
-        </form>
-        <div style="clear: both"></div>
+<h1>Widget manager</h1>
+<form name="main_form" method="post">
+    <div class="container bot-shadow">
+        <input id="all-check" onclick="mm_all_check_click();" style="margin-left: 13px;" type="checkbox" />
+        <input onclick="history.back();" class="btn-gray" name="back" type="button" value="◄ Back" />
+        <input class="btn-green" onclick="location = '?p=<?php echo Application::$Configuration->AdminPage; ?>&open=side_manager_add'" name="nav_add" type="button" value="✚ Add" />
+        <input id="delete-selected" class="btn-gray" style="float: right" onclick="" name="delete-selected" type="button" value="✖ Delete" />
     </div>
-<?php endforeach; ?>
+
+    <div class="container box-scrolling">
+        <?php  $index = 0; foreach (Application::$Database->SideItems as $SideItem): $index++; ?>
+        <div name="sideitem" class="sideitem">     
+                <div class="sideitem-col">
+                    <input id="sideitem-check<?php echo $index; ?>" onchange="mm_sideitem_check_onchange();" name="side-item-select_<?php echo $SideItem->getValue()->SideItemId; ?>" type="checkbox" />
+                    <?php if ($this->ChangeSideItemId == $SideItem->getValue()->SideItemId): ?>
+                        <input class="tbx"  type="text" name="side-item-title_<?php echo $SideItem->getValue()->SideItemId; ?>" value="<?php echo $SideItem->getValue()->Title; ?>" />
+                    <?php else: ?>
+                        <span><?php echo $SideItem->getValue()->Title; ?></span>
+                    <?php endif; ?>
+                    <div style="clear: both"></div>
+                </div>
+                <div class="sideitem-nav">
+                    <?php if ($this->ChangeSideItemId == $SideItem->getValue()->SideItemId): ?>
+                        <input class="btn-green" name="side-item-save_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="✔" />
+                    <?php else: ?>
+                        <input class="btn-gray" name="side-item-change_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="✎" />
+                    <?php endif; ?>
+                    <input class="btn-gray" name="side-item-inc_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="▲" />
+                    <input class="btn-gray" name="side-item-dec_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="▼" />
+                    <input class="btn-red" name="side-item-del_<?php echo $SideItem->getValue()->SideItemId; ?>" type="submit" value="✖" /> 
+                </div>
+
+            <div style="clear: both"></div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</form>
--- App/Views/Footer.php
+++ App/Views/Footer.php
@@ -1,2 +1,7 @@
 <?php use App\Application as Application; ?>
-
+                <div style="clear: both;"></div>
+            </div>
+            <div style="clear: both;"></div>
+        </div>
+    </body>
+</html>
